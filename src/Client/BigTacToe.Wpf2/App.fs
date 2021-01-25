82d016ea68c1b2e8008684eca5a410fdec168c97
Ito, Kai <kai.ito@zuehlke.com>
2021-01-25 09:12:05 +0100

    Add second WPF project

---
+++ src/Client/BigTacToe.Wpf2/App.fs
@@ -0,0 +1,21 @@
+﻿namespace BigTacToe.Wpf
+
+open System
+
+open Xamarin.Forms
+open Xamarin.Forms.Platform.WPF
+
+type MainWindow() = 
+    inherit FormsApplicationPage()
+
+module Main = 
+    [<EntryPoint>]
+    [<STAThread>]
+    let main(_args) =
+
+        let app = new System.Windows.Application()
+        Forms.Init()
+        let window = MainWindow() 
+        window.LoadApplication(BigTacToe.App())
+
+        app.Run(window)
---
+++ src/Client/BigTacToe.Wpf2/Properties/AssemblyInfo.fs
@@ -0,0 +1,24 @@
+﻿namespace BigTacToe.Wpf.WPF
+open System.Reflection
+open System.Runtime.CompilerServices
+
+// the name of the type here needs to match the name inside the ResourceDesigner attribute
+//type Resources = BigTacToe.Wpf.Droid.Resource
+//[<assembly: Android.Runtime.ResourceDesigner("BigTacToe.Wpf.WPF.Resources", IsApplication=true)>]
+
+[<assembly: AssemblyTitle("BigTacToe.Wpf.WPF")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
